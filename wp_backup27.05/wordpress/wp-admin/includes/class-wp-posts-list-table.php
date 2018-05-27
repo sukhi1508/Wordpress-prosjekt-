09dbbf905a58706945b4d8e56248336dae0779ec
Mercedes Beanz <benjamin.tornes@gmail.com>
2018-05-27 21:06:27 +0200

    backup 3

---
+++ wp_backup27.05/wordpress/wp-admin/includes/class-wp-posts-list-table.php
@@ -0,0 +1,572 @@
+<?php
+/**
+ * List Table API: WP_Posts_List_Table class
+ *
+ * @package WordPress
+ * @subpackage Administration
+ * @since 3.1.0
+ */
+
+/**
+ * Core class used to implement displaying posts in a list table.
+ *
+ * @since 3.1.0
+ * @access private
+ *
+ * @see WP_List_Table
+ */
+class WP_Posts_List_Table extends WP_List_Table {
+
+	/**
+	 * Whether the items should be displayed hierarchically or linearly.
+	 *
+	 * @since 3.1.0
+	 * @var bool
+	 */
+	protected $hierarchical_display;
+
+	/**
+	 * Holds the number of pending comments for each post.
+	 *
+	 * @since 3.1.0
+	 * @var array
+	 */
+	protected $comment_pending_count;
+
+	/**
+	 * Holds the number of posts for this user.
+	 *
+	 * @since 3.1.0
+	 * @var int
+	 */
+	private $user_posts_count;
+
+	/**
+	 * Holds the number of posts which are sticky.
+	 *
+	 * @since 3.1.0
+	 * @var int
+	 */
+	private $sticky_posts_count = 0;
+
+	private $is_trash;
+
+	/**
+	 * Current level for output.
+	 *
+	 * @since 4.3.0
+	 * @var int
+	 */
+	protected $current_level = 0;
+
+	/**
+	 * Constructor.
+	 *
+	 * @since 3.1.0
+	 *
+	 * @see WP_List_Table::__construct() for more information on default arguments.
+	 *
+	 * @global WP_Post_Type $post_type_object
+	 * @global wpdb         $wpdb
+	 *
+	 * @param array $args An associative array of arguments.
+	 */
+	public function __construct( $args = array() ) {
+		global $post_type_object, $wpdb;
+
+		parent::__construct( array(
+			'plural' => 'posts',
+			'screen' => isset( $args['screen'] ) ? $args['screen'] : null,
+		) );
+
+		$post_type        = $this->screen->post_type;
+		$post_type_object = get_post_type_object( $post_type );
+
+		$exclude_states   = get_post_stati( array(
+			'show_in_admin_all_list' => false,
+		) );
+		$this->user_posts_count = intval( $wpdb->get_var( $wpdb->prepare( "
+			SELECT COUNT( 1 )
+			FROM $wpdb->posts
+			WHERE post_type = %s
+			AND post_status NOT IN ( '" . implode( "','", $exclude_states ) . "' )
+			AND post_author = %d
+		", $post_type, get_current_user_id() ) ) );
+
+		if ( $this->user_posts_count && ! current_user_can( $post_type_object->cap->edit_others_posts ) && empty( $_REQUEST['post_status'] ) && empty( $_REQUEST['all_posts'] ) && empty( $_REQUEST['author'] ) && empty( $_REQUEST['show_sticky'] ) ) {
+			$_GET['author'] = get_current_user_id();
+		}
+
+		if ( 'post' === $post_type && $sticky_posts = get_option( 'sticky_posts' ) ) {
+			$sticky_posts = implode( ', ', array_map( 'absint', (array) $sticky_posts ) );
+			$this->sticky_posts_count = $wpdb->get_var( $wpdb->prepare( "SELECT COUNT( 1 ) FROM $wpdb->posts WHERE post_type = %s AND post_status NOT IN ('trash', 'auto-draft') AND ID IN ($sticky_posts)", $post_type ) );
+		}
+	}
+
+	/**
+	 * Sets whether the table layout should be hierarchical or not.
+	 *
+	 * @since 4.2.0
+	 *
+	 * @param bool $display Whether the table layout should be hierarchical.
+	 */
+	public function set_hierarchical_display( $display ) {
+		$this->hierarchical_display = $display;
+	}
+
+	/**
+	 *
+	 * @return bool
+	 */
+	public function ajax_user_can() {
+		return current_user_can( get_post_type_object( $this->screen->post_type )->cap->edit_posts );
+	}
+
+	/**
+	 *
+	 * @global array    $avail_post_stati
+	 * @global WP_Query $wp_query
+	 * @global int      $per_page
+	 * @global string   $mode
+	 */
+	public function prepare_items() {
+		global $avail_post_stati, $wp_query, $per_page, $mode;
+
+		// is going to call wp()
+		$avail_post_stati = wp_edit_posts_query();
+
+		$this->set_hierarchical_display( is_post_type_hierarchical( $this->screen->post_type ) && 'menu_order title' === $wp_query->query['orderby'] );
+
+		$post_type = $this->screen->post_type;
+		$per_page = $this->get_items_per_page( 'edit_' . $post_type . '_per_page' );
+
+		/** This filter is documented in wp-admin/includes/post.php */
+ 		$per_page = apply_filters( 'edit_posts_per_page', $per_page, $post_type );
+
+		if ( $this->hierarchical_display ) {
+			$total_items = $wp_query->post_count;
+		} elseif ( $wp_query->found_posts || $this->get_pagenum() === 1 ) {
+			$total_items = $wp_query->found_posts;
+		} else {
+			$post_counts = (array) wp_count_posts( $post_type, 'readable' );
+
+			if ( isset( $_REQUEST['post_status'] ) && in_array( $_REQUEST['post_status'] , $avail_post_stati ) ) {
+				$total_items = $post_counts[ $_REQUEST['post_status'] ];
+			} elseif ( isset( $_REQUEST['show_sticky'] ) && $_REQUEST['show_sticky'] ) {
+				$total_items = $this->sticky_posts_count;
+			} elseif ( isset( $_GET['author'] ) && $_GET['author'] == get_current_user_id() ) {
+				$total_items = $this->user_posts_count;
+			} else {
+				$total_items = array_sum( $post_counts );
+
+				// Subtract post types that are not included in the admin all list.
+				foreach ( get_post_stati( array( 'show_in_admin_all_list' => false ) ) as $state ) {
+					$total_items -= $post_counts[ $state ];
+				}
+			}
+		}
+
+		if ( ! empty( $_REQUEST['mode'] ) ) {
+			$mode = $_REQUEST['mode'] === 'excerpt' ? 'excerpt' : 'list';
+			set_user_setting( 'posts_list_mode', $mode );
+		} else {
+			$mode = get_user_setting( 'posts_list_mode', 'list' );
+		}
+
+		$this->is_trash = isset( $_REQUEST['post_status'] ) && $_REQUEST['post_status'] === 'trash';
+
+		$this->set_pagination_args( array(
+			'total_items' => $total_items,
+			'per_page' => $per_page
+		) );
+	}
+
+	/**
+	 *
+	 * @return bool
+	 */
+	public function has_items() {
+		return have_posts();
+	}
+
+	/**
+	 */
+	public function no_items() {
+		if ( isset( $_REQUEST['post_status'] ) && 'trash' === $_REQUEST['post_status'] )
+			echo get_post_type_object( $this->screen->post_type )->labels->not_found_in_trash;
+		else
+			echo get_post_type_object( $this->screen->post_type )->labels->not_found;
+	}
+
+	/**
+	 * Determine if the current view is the "All" view.
+	 *
+	 * @since 4.2.0
+	 *
+	 * @return bool Whether the current view is the "All" view.
+	 */
+	protected function is_base_request() {
+		$vars = $_GET;
+		unset( $vars['paged'] );
+
+		if ( empty( $vars ) ) {
+			return true;
+		} elseif ( 1 === count( $vars ) && ! empty( $vars['post_type'] ) ) {
+			return $this->screen->post_type === $vars['post_type'];
+		}
+
+		return 1 === count( $vars ) && ! empty( $vars['mode'] );
+	}
+
+	/**
+	 * Helper to create links to edit.php with params.
+	 *
+	 * @since 4.4.0
+	 *
+	 * @param array  $args  URL parameters for the link.
+	 * @param string $label Link text.
+	 * @param string $class Optional. Class attribute. Default empty string.
+	 * @return string The formatted link string.
+	 */
+	protected function get_edit_link( $args, $label, $class = '' ) {
+		$url = add_query_arg( $args, 'edit.php' );
+
+		$class_html = $aria_current = '';
+		if ( ! empty( $class ) ) {
+			 $class_html = sprintf(
+				' class="%s"',
+				esc_attr( $class )
+			);
+
+			if ( 'current' === $class ) {
+				$aria_current = ' aria-current="page"';
+			}
+		}
+
+		return sprintf(
+			'<a href="%s"%s%s>%s</a>',
+			esc_url( $url ),
+			$class_html,
+			$aria_current,
+			$label
+		);
+	}
+
+	/**
+	 *
+	 * @global array $locked_post_status This seems to be deprecated.
+	 * @global array $avail_post_stati
+	 * @return array
+	 */
+	protected function get_views() {
+		global $locked_post_status, $avail_post_stati;
+
+		$post_type = $this->screen->post_type;
+
+		if ( !empty($locked_post_status) )
+			return array();
+
+		$status_links = array();
+		$num_posts = wp_count_posts( $post_type, 'readable' );
+		$total_posts = array_sum( (array) $num_posts );
+		$class = '';
+
+		$current_user_id = get_current_user_id();
+		$all_args = array( 'post_type' => $post_type );
+		$mine = '';
+
+		// Subtract post types that are not included in the admin all list.
+		foreach ( get_post_stati( array( 'show_in_admin_all_list' => false ) ) as $state ) {
+			$total_posts -= $num_posts->$state;
+		}
+
+		if ( $this->user_posts_count && $this->user_posts_count !== $total_posts ) {
+			if ( isset( $_GET['author'] ) && ( $_GET['author'] == $current_user_id ) ) {
+				$class = 'current';
+			}
+
+			$mine_args = array(
+				'post_type' => $post_type,
+				'author' => $current_user_id
+			);
+
+			$mine_inner_html = sprintf(
+				_nx(
+					'Mine <span class="count">(%s)</span>',
+					'Mine <span class="count">(%s)</span>',
+					$this->user_posts_count,
+					'posts'
+				),
+				number_format_i18n( $this->user_posts_count )
+			);
+
+			$mine = $this->get_edit_link( $mine_args, $mine_inner_html, $class );
+
+			$all_args['all_posts'] = 1;
+			$class = '';
+		}
+
+		if ( empty( $class ) && ( $this->is_base_request() || isset( $_REQUEST['all_posts'] ) ) ) {
+			$class = 'current';
+		}
+
+		$all_inner_html = sprintf(
+			_nx(
+				'All <span class="count">(%s)</span>',
+				'All <span class="count">(%s)</span>',
+				$total_posts,
+				'posts'
+			),
+			number_format_i18n( $total_posts )
+		);
+
+		$status_links['all'] = $this->get_edit_link( $all_args, $all_inner_html, $class );
+		if ( $mine ) {
+			$status_links['mine'] = $mine;
+		}
+
+		foreach ( get_post_stati(array('show_in_admin_status_list' => true), 'objects') as $status ) {
+			$class = '';
+
+			$status_name = $status->name;
+
+			if ( ! in_array( $status_name, $avail_post_stati ) || empty( $num_posts->$status_name ) ) {
+				continue;
+			}
+
+			if ( isset($_REQUEST['post_status']) && $status_name === $_REQUEST['post_status'] ) {
+				$class = 'current';
+			}
+
+			$status_args = array(
+				'post_status' => $status_name,
+				'post_type' => $post_type,
+			);
+
+			$status_label = sprintf(
+				translate_nooped_plural( $status->label_count, $num_posts->$status_name ),
+				number_format_i18n( $num_posts->$status_name )
+			);
+
+			$status_links[ $status_name ] = $this->get_edit_link( $status_args, $status_label, $class );
+		}
+
+		if ( ! empty( $this->sticky_posts_count ) ) {
+			$class = ! empty( $_REQUEST['show_sticky'] ) ? 'current' : '';
+
+			$sticky_args = array(
+				'post_type'	=> $post_type,
+				'show_sticky' => 1
+			);
+
+			$sticky_inner_html = sprintf(
+				_nx(
+					'Sticky <span class="count">(%s)</span>',
+					'Sticky <span class="count">(%s)</span>',
+					$this->sticky_posts_count,
+					'posts'
+				),
+				number_format_i18n( $this->sticky_posts_count )
+			);
+
+			$sticky_link = array(
+				'sticky' => $this->get_edit_link( $sticky_args, $sticky_inner_html, $class )
+			);
+
+			// Sticky comes after Publish, or if not listed, after All.
+			$split = 1 + array_search( ( isset( $status_links['publish'] ) ? 'publish' : 'all' ), array_keys( $status_links ) );
+			$status_links = array_merge( array_slice( $status_links, 0, $split ), $sticky_link, array_slice( $status_links, $split ) );
+		}
+
+		return $status_links;
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	protected function get_bulk_actions() {
+		$actions = array();
+		$post_type_obj = get_post_type_object( $this->screen->post_type );
+
+		if ( current_user_can( $post_type_obj->cap->edit_posts ) ) {
+			if ( $this->is_trash ) {
+				$actions['untrash'] = __( 'Restore' );
+			} else {
+				$actions['edit'] = __( 'Edit' );
+			}
+		}
+
+		if ( current_user_can( $post_type_obj->cap->delete_posts ) ) {
+			if ( $this->is_trash || ! EMPTY_TRASH_DAYS ) {
+				$actions['delete'] = __( 'Delete Permanently' );
+			} else {
+				$actions['trash'] = __( 'Move to Trash' );
+			}
+		}
+
+		return $actions;
+	}
+
+	/**
+	 * Displays a categories drop-down for filtering on the Posts list table.
+	 *
+	 * @since 4.6.0
+	 *
+	 * @global int $cat Currently selected category.
+	 *
+	 * @param string $post_type Post type slug.
+	 */
+	protected function categories_dropdown( $post_type ) {
+		global $cat;
+
+		/**
+		 * Filters whether to remove the 'Categories' drop-down from the post list table.
+		 *
+		 * @since 4.6.0
+		 *
+		 * @param bool   $disable   Whether to disable the categories drop-down. Default false.
+		 * @param string $post_type Post type slug.
+		 */
+		if ( false !== apply_filters( 'disable_categories_dropdown', false, $post_type ) ) {
+			return;
+		}
+
+		if ( is_object_in_taxonomy( $post_type, 'category' ) ) {
+			$dropdown_options = array(
+				'show_option_all' => get_taxonomy( 'category' )->labels->all_items,
+				'hide_empty' => 0,
+				'hierarchical' => 1,
+				'show_count' => 0,
+				'orderby' => 'name',
+				'selected' => $cat
+			);
+
+			echo '<label class="screen-reader-text" for="cat">' . __( 'Filter by category' ) . '</label>';
+			wp_dropdown_categories( $dropdown_options );
+		}
+	}
+
+	/**
+	 * @param string $which
+	 */
+	protected function extra_tablenav( $which ) {
+?>
+		<div class="alignleft actions">
+<?php
+		if ( 'top' === $which && !is_singular() ) {
+			ob_start();
+
+			$this->months_dropdown( $this->screen->post_type );
+			$this->categories_dropdown( $this->screen->post_type );
+
+			/**
+			 * Fires before the Filter button on the Posts and Pages list tables.
+			 *
+			 * The Filter button allows sorting by date and/or category on the
+			 * Posts list table, and sorting by date on the Pages list table.
+			 *
+			 * @since 2.1.0
+			 * @since 4.4.0 The `$post_type` parameter was added.
+			 * @since 4.6.0 The `$which` parameter was added.
+			 *
+			 * @param string $post_type The post type slug.
+			 * @param string $which     The location of the extra table nav markup:
+			 *                          'top' or 'bottom' for WP_Posts_List_Table,
+			 *                          'bar' for WP_Media_List_Table.
+			 */
+			do_action( 'restrict_manage_posts', $this->screen->post_type, $which );
+
+			$output = ob_get_clean();
+
+			if ( ! empty( $output ) ) {
+				echo $output;
+				submit_button( __( 'Filter' ), '', 'filter_action', false, array( 'id' => 'post-query-submit' ) );
+			}
+		}
+
+		if ( $this->is_trash && current_user_can( get_post_type_object( $this->screen->post_type )->cap->edit_others_posts ) && $this->has_items() ) {
+			submit_button( __( 'Empty Trash' ), 'apply', 'delete_all', false );
+		}
+?>
+		</div>
+<?php
+		/**
+		 * Fires immediately following the closing "actions" div in the tablenav for the posts
+		 * list table.
+		 *
+		 * @since 4.4.0
+		 *
+		 * @param string $which The location of the extra table nav markup: 'top' or 'bottom'.
+		 */
+		do_action( 'manage_posts_extra_tablenav', $which );
+	}
+
+	/**
+	 *
+	 * @return string
+	 */
+	public function current_action() {
+		if ( isset( $_REQUEST['delete_all'] ) || isset( $_REQUEST['delete_all2'] ) )
+			return 'delete_all';
+
+		return parent::current_action();
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	protected function get_table_classes() {
+		return array( 'widefat', 'fixed', 'striped', is_post_type_hierarchical( $this->screen->post_type ) ? 'pages' : 'posts' );
+	}
+
+	/**
+	 *
+	 * @return array
+	 */
+	public function get_columns() {
+		$post_type = $this->screen->post_type;
+
+		$posts_columns = array();
+
+		$posts_columns['cb'] = '<input type="checkbox" />';
+
+		/* translators: manage posts column name */
+		$posts_columns['title'] = _x( 'Title', 'column name' );
+
+		if ( post_type_supports( $post_type, 'author' ) ) {
+			$posts_columns['author'] = __( 'Author' );
+		}
+
+		$taxonomies = get_object_taxonomies( $post_type, 'objects' );
+		$taxonomies = wp_filter_object_list( $taxonomies, array( 'show_admin_column' => true ), 'and', 'name' );
+
+		/**
+		 * Filters the taxonomy columns in the Posts list table.
+		 *
+		 * The dynamic portion of the hook name, `$post_type`, refers to the post
+		 * type slug.
+		 *
+		 * @since 3.5.0
+		 *
+		 * @param array  $taxonomies Array of taxonomies to show columns for.
+		 * @param string $post_type  The post type.
+		 */
+		$taxonomies = apply_filters( "manage_taxonomies_for_{$post_type}_columns", $taxonomies, $post_type );
+		$taxonomies = array_filter( $taxonomies, 'taxonomy_exists' );
+
+		foreach ( $taxonomies as $taxonomy ) {
+			if ( 'category' === $taxonomy )
+				$column_key = 'categories';
+			elseif ( 'post_tag' === $taxonomy )
+				$column_key = 'tags';
+			else
+				$column_key = 'taxonomy-' . $taxonomy;
+
+			$posts_columns[ $column_key ] = get_taxonomy( $taxonomy )->labels->name;
+		}
+
+		$post_status = !empty( $_REQUEST['post_status'] ) ? $_REQUEST['post_status'] : 'all';
+		if ( post_type_supports( $post_type, 'comments' ) && !in_array( $post_status, array( 'pending', 'draft', 'future' ) ) )
+			$posts_columns['comments'] = '<span 
